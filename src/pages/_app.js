aa92a58fb0a79e61701ababd0538b21d7c8ef4bb
Mariana Trujillo Islas <105942792+MarianaTI@users.noreply.github.com>
2025-09-08 21:36:47 -0600

    changes

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,18 @@
 import "@/styles/globals.css";
+import { Geist, Geist_Mono, Poppins } from "next/font/google";
+
+const poppins = Poppins({
+  weight: ["400", "500", "600", "700", "800"],
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-poppins",
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div className={poppins.className}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
