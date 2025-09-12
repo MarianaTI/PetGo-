742ae679e11b8fb79ca6157491b2d285fd55723c
Mariana Trujillo Islas <105942792+MarianaTI@users.noreply.github.com>
2025-09-11 22:56:38 -0600

    changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,90 +1,44 @@
-import React from 'react'
+import React from "react";
 
 export default function Header() {
   return (
-    <header class="fbs__net-navbar navbar navbar-expand-lg dark" aria-label="freebootstrap.net navbar">
-        <div class="container d-flex align-items-center justify-content-between">
-          
-
-          <a class="navbar-brand w-auto">
-            <img src='/logo.ico'/>
-          </a>
-
-          <div class="offcanvas offcanvas-start w-75" id="fbs__net-navbars" tabindex="-1" aria-labelledby="fbs__net-navbarsLabel">
-            
-            
-            <div class="offcanvas-header">
-              <div class="offcanvas-header-logo">
-                
-              </div>
-              <button class="btn-close btn-close-black" type="button" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-            </div>
-            
-            <div class="offcanvas-body align-items-lg-center">
-              
-              
-              <ul class="navbar-nav nav me-auto ps-lg-5 mb-2 mb-lg-0">
-                <li class="nav-item"><a class="nav-link scroll-link active" aria-current="page" href="#home">Home</a></li>
-                <li class="nav-item"><a class="nav-link scroll-link" href="#about">About</a></li>
-                <li class="nav-item"><a class="nav-link scroll-link" href="#pricing">Pricing</a></li>
-                <li class="nav-item"><a class="nav-link scroll-link" href="#how-it-works">How It Works</a></li>
-                <li class="nav-item"><a class="nav-link scroll-link" href="#services">Services</a></li>
-                <li class="nav-item dropdown"><a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false">Dropdown <i class="bi bi-chevron-down"></i></a>
-                  
-                  <ul class="dropdown-menu">
-                    <li><a class="nav-link scroll-link dropdown-item" href="#">Multipages</a></li>
-                    <li><a class="nav-link scroll-link dropdown-item" href="#services">Services</a></li>
-                    <li><a class="nav-link scroll-link dropdown-item" href="#pricing">Pricing</a></li>
-                    <li class="nav-item dropstart"><a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false">Dropstart <i class="bi bi-chevron-right"></i></a>
-                      <ul class="dropdown-menu">
-                        <li><a class="nav-link scroll-link dropdown-item" href="#services">Services</a></li>
-                        <li><a class="nav-link scroll-link dropdown-item" href="#pricing">Pricing</a></li>
-                        <li class="nav-item dropstart"><a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false">Dropstart <i class="bi bi-chevron-right"></i></a>
-                          <ul class="dropdown-menu">
-                            <li><a class="nav-link scroll-link dropdown-item" href="#services">Services</a></li>
-                            <li><a class="nav-link scroll-link dropdown-item" href="#pricing">Pricing</a></li>
-                            <li><a class="nav-link scroll-link dropdown-item" href="#">Something else here</a></li>
-                            <li class="nav-item dropend"><a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false">Dropend <i class="bi bi-chevron-right"></i></a>
-                              <ul class="dropdown-menu">
-                                <li><a class="nav-link scroll-link dropdown-item" href="#services">Services</a></li>
-                                <li><a class="nav-link scroll-link dropdown-item" href="#pricing">Pricing</a></li>
-                                <li><a class="nav-link scroll-link dropdown-item" href="#">Something else here</a></li>
-                              </ul>
-                            </li>
-                          </ul>
-                        </li>
-                      </ul>
-                    </li>
-                  </ul>
-                  
-                </li>
-                <li class="nav-item"><a class="nav-link scroll-link" href="#contact">Contact</a></li>
-              </ul>
-              
-            </div>
-          </div>
-          
-          <div class="ms-auto w-auto">
-            
-            
-            <div class="header-social d-flex align-items-center gap-1"><a class="btn btn-primary py-2" href="#">Get Started</a>
-              
-              <button class="fbs__net-navbar-toggler justify-content-center align-items-center ms-auto" data-bs-toggle="offcanvas" data-bs-target="#fbs__net-navbars" aria-controls="fbs__net-navbars" aria-label="Toggle navigation" aria-expanded="false">
-                <svg class="fbs__net-icon-menu" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewbox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                  <line x1="21" x2="3" y1="6" y2="6"></line>
-                  <line x1="15" x2="3" y1="12" y2="12"></line>
-                  <line x1="17" x2="3" y1="18" y2="18"></line>
-                </svg>
-                <svg class="fbs__net-icon-close" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewbox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                  <path d="M18 6 6 18"></path>
-                  <path d="m6 6 12 12"></path>
-                </svg>
-              </button>
-              
-            </div>
-            
-          </div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+          Navbar w/ text
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarText"
+          aria-controls="navbarText"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarText">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">
+                Home
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Features
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Pricing
+              </a>
+            </li>
+          </ul>
+          <span class="navbar-text">Navbar text with an inline element</span>
         </div>
-      </header>
-  )
+      </div>
+    </nav>
+  );
 }
